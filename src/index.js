3f65b20d07fb9e68dfdb2e7c2fda19f8e9025edb
dkiziryan <dkiziryan@gmail.com>
2019-07-14 23:29:05 -0700

    clean up

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
 import store from "./store";
-// import * as actions from "./redux/actions";
 
 ReactDOM.render(
   <Provider store={store}>
